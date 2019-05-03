363549e82845705c535b2437cb22105677cf515f
Wayne Stedman <wstedman@dhccare.com>
2019-05-03 10:00:48 -0700

    desktop styling; 2 testimonial setup

--- dest/js/vsc.js
+++ dest/js/vsc.js
@@ -82,15 +82,25 @@ $(document).ready(function () {
   initComparisons();
   $('#testimonialsSlider').slick({
     dots: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    fade: true,
+    // arrows: true,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
+    // fade: true,
+    focusOnChange: true,
     speed: 1200,
     // cssEase: 'ease-in-out',
     infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    swipe: true
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    swipe: true,
+    responsive: [{
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    }]
   }); // testimonials slider
 }); //doc ready
 
